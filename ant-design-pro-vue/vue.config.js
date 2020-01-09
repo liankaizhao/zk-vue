5c529032d760c9b7f8f4a772950ea23c9149434c
br <17816873148@163.com>
2020-01-09 10:54:43 +0800

    first commit

---
+++ ant-design-pro-vue/vue.config.js
@@ -0,0 +1,88 @@
+const path = require('path')
+const webpack = require('webpack')
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+
+function resolve (dir) {
+  return path.join(__dirname, dir)
+}
+
+// vue.config.js
+module.exports = {
+  /*
+    Vue-cli3:
+    Crashed when using Webpack `import()` #2463
+    https://github.com/vuejs/vue-cli/issues/2463
+
+   */
+  /*
+  pages: {
+    index: {
+      entry: 'src/main.js',
+      chunks: ['chunk-vendors', 'chunk-common', 'index']
+    }
+  },
+  */
+  configureWebpack: {
+    plugins: [
+      // Ignore all locale files of moment.js
+      new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+      new BundleAnalyzerPlugin()
+    ],
+    externals: {
+      'vue': 'Vue',
+      'ant-design-vue': 'antd'
+    }
+  },
+
+  chainWebpack: (config) => {
+    config.resolve.alias
+      .set('@$', resolve('src'))
+      .set('@api', resolve('src/api'))
+      .set('@assets', resolve('src/assets'))
+      .set('@comp', resolve('src/components'))
+      .set('@views', resolve('src/views'))
+      .set('@layout', resolve('src/layout'))
+      .set('@static', resolve('src/static'))
+  },
+
+  css: {
+    loaderOptions: {
+      less: {
+        modifyVars: {
+          /* less 变量覆盖，用于自定义 ant design 主题 */
+
+          /*
+          'primary-color': '#F5222D',
+          'link-color': '#F5222D',
+          'border-radius-base': '4px',
+          */
+          'font-size-base': '13px'
+        },
+        javascriptEnabled: true
+      }
+    }
+  },
+
+  devServer: {
+    disableHostCheck: true,
+    proxy: {
+      '/api': {
+        // target: 'https://mock.ihx.me/mock/5baf3052f7da7e07e04a5116/antd-pro',
+        target: 'http://10.60.45.69:8080',
+        ws: false,
+        changeOrigin: true
+      },
+      '/gateway': {
+        target: 'http://10.60.45.69:8080',
+        ws: false,
+        changeOrigin: true,
+        pathRewrite: {
+          '^/gateway': '/api'
+        }
+      }
+    }
+  },
+
+  lintOnSave: undefined,
+  productionSourceMap: false
+}
